29123fee684e500057d36fa298710329dc91aefa
bryres <bryres@bergen.org>
2015-06-09 10:32:41 -0400

    added input

--- src/org/bca/Hello.java
+++ src/org/bca/Hello.java
@@ -1,9 +1,16 @@
 package org.bca;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello - B!");
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("What is your name?");
 
+		String name = input.nextLine();
+		
+		System.out.println("Hello - " + name);
 	}
 }
